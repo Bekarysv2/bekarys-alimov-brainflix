2815e8efceba373ff0863a4ae04a9b6a470457da
Bekarys Alimov <beka.alimov2000@gmail.com>
2024-03-12 16:43:54 +0000

    Updated js code from sprint 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import {useState} from 'react';
 import './App.css';
+import NavigationBar from './components/Navigation-Bar/Nav-bar';
+import MainVideo from './components/MainVideo/Main-video';
+import VideoDetails from './components/VideoDetails/VideoDetails';
+import Comments from './components/Comments/Comments';
+import NewComment from './components/Add-Comment/New-comment';
+import Videolist from './components/Video-List/Video-list';
+
+import VideoJson from './data/video-details.json';
 
 function App() {
+  const [videos, setVideos] = useState(VideoJson)
+  const [selectedVideo, setSelectedVideo] = useState(VideoJson[0]);
+  const [comments, setComments] = useState(VideoJson[0].comments);
+
+
+  const clickHandler = (video) => {
+    setSelectedVideo(video);
+  }
+
   return (
-    <div>Hello</div>
+    <>
+      <NavigationBar />
+      <MainVideo video={selectedVideo} />
+      <VideoDetails video={selectedVideo} />
+      <NewComment commentsCount={comments.length} />
+      <Comments video={selectedVideo} />
+      <Videolist videos={videos} clickHandler={clickHandler} selectedVideo={selectedVideo} />
+    </>
   );
 }
+
 
 export default App;
---
+++ src/components/Navigation-Bar/Nav-bar.js
@@ -0,0 +1,32 @@
+import './Nav-bar.scss';
+import logo from '../../assets/logo/BrainFlix-logo.svg';
+import mohanImg from '../../assets/images/Mohan-muruge.jpg';
+import React, { useState } from 'react';
+
+const NavigationBar = () => {
+
+  return (
+    <nav className="nav">
+        <img src={logo} alt="BrainFlix Logo" className="nav__logo" />
+      <div className="nav__container">
+          <input
+            type="search"
+            placeholder="Search"
+            className="nav__search"
+            id="search-input"
+          ></input>
+        <button type="submit" className="nav__upload-button">
+        UPLOAD
+        </button>
+        <div className="nav__container__profile">
+          <img src={mohanImg} alt="Mohan-profile" className="nav__mohan"></img>
+        </div>
+      </div>
+      <button type="submit" className="nav__upload-button-mobile">
+        UPLOAD
+      </button>
+    </nav>
+  );
+}
+
+export default NavigationBar;
