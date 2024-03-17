bff18a976f2d524ff4f695da9807f982ee82b88a
Bekarys Alimov <beka.alimov2000@gmail.com>
2024-03-17 21:56:42 +0000

    fixed navigation bar style

--- src/components/Navigation-Bar/Nav-bar.js
+++ src/components/Navigation-Bar/Nav-bar.js
@@ -11,12 +11,14 @@ const NavigationBar = () => {
             <img src={logo} alt="BrainFlix Logo" className="nav__logo" />
         </Link>
       <div className="nav__container">
+        <div className="nav__container__searchBar">
           <input
             type="search"
             placeholder="Search"
             className="nav__search"
             id="search-input"
           ></input>
+        </div>
         <Link to="/upload" className="nav__upload-button">UPLOAD</Link> 
         <div className="nav__container__profile">
           <img src={mohanImg} alt="Mohan profile" className="nav__mohan"></img>
