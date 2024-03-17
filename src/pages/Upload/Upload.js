201c79c4a64049f6659dc404077977439964942b
Bekarys Alimov <beka.alimov2000@gmail.com>
2024-03-17 21:43:18 +0000

    added Upload-page js and scss

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -17,35 +17,40 @@ const UploadPage = () => {
     }
 
     return (
-        <div className="upload-wrapper">
-          <h1 className="upload-wrapper__title">Upload Video</h1>
-          <p className="upload-wrapper__thumbtitle">VIDEO THUMBNAIL</p>
-          <img src={thumbnailImage} alt="Preview" className="upload-wrapper__thumbnail" />
-          
-          <form className="upload-wrapper__form" onSubmit={handleSubmit}>
-            <label htmlFor="videoTitle" className="upload-wrapper__label">TITLE YOUR VIDEO</label>
-            <input
-              id="videoTitle"
-              name="videoTitle"
-              className="upload-wrapper__input"
-              placeholder="Add a title to your video"
-              required
-            />
-    
-            <label htmlFor="videoDesc" className="upload-wrapper__label">ADD A VIDEO DESCRIPTION</label>
-            <textarea
-              id="videoDesc"
-              name="videoDesc"
-              className="upload-wrapper__textarea"
-              placeholder="Add a description to your video"
-              required
-            ></textarea>
-    
-            <div className="upload-wrapper__actions">
-              <button type="button" className="upload-wrapper__cancel" onClick={handleCancel}>Cancel</button>
-              <button type="submit" className="upload-wrapper__submit">PUBLISH</button>
+        <div className="upload">
+          <p className="upload__title">Upload Video</p>
+          <div className="upload__container">
+            <div className="upload__desktop-flex">
+                <p className="upload__thumbtitle">VIDEO THUMBNAIL</p>
+                <img src={thumbnailImage} alt="Preview" className="upload__thumbnail" />
             </div>
-          </form>
+            
+            <form className="upload__form" onSubmit={handleSubmit}>
+                <label htmlFor="videoTitle" className="upload__label">TITLE YOUR VIDEO</label>
+                <input
+                id="videoTitle"
+                name="videoTitle"
+                className="upload__input"
+                placeholder="Add a title to your video"
+                required
+                />
+        
+                <label htmlFor="videoDesc" className="upload__label">ADD A VIDEO DESCRIPTION</label>
+                <textarea
+                id="videoDesc"
+                name="videoDesc"
+                className="upload__textarea"
+                placeholder="Add a description to your video"
+                required
+                ></textarea>
+            </form>
+            </div>
+
+    
+            <div className="upload__buttons">
+              <button type="button" className="upload__buttons__cancel" onClick={handleCancel}>CANCEL</button>
+              <button type="submit" className="upload__buttons__submit">PUBLISH</button>
+            </div>
         </div>
       );
     };
