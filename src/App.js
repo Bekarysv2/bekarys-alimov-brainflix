1c01ea6309deb8e5c76349a1512db03b95a991c3
Bekarys Alimov <bekarysalimov@Bekaryss-MacBook-Pro.local>
2024-04-01 02:05:34 +0100

    fixed App.js page

--- src/App.js
+++ src/App.js
@@ -6,26 +6,15 @@ import NavigationBar from './components/Navigation-Bar/Nav-bar';
 import Home from './pages/Home/Home'; 
 import UploadPage from './pages/Upload/Upload'; 
 
-const api = "https://unit-3-project-api-0a5620414506.herokuapp.com";
-const apiKey = "b54c779d-f50f-4e7f-835a-a08775c541a5";
 
 function App() {
-  const [videos, setVideos] = useState([]);
-
-  useEffect(() => {
-    axios.get(`${api}/videos?api_key=${apiKey}`)
-      .then(response => {
-        setVideos(response.data);
-      })
-      .catch(error => console.error("Failed to fetch videos", error));
-  }, []);
 
   return (
     <BrowserRouter>
       <NavigationBar className="nav-bar" />
       <Routes>
-        <Route path="/" element={<Home videos={videos} />} />
-        <Route path="/video/:videoId" element={<Home videos={videos} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/video/:videoId" element={<Home />} />
         <Route path="/upload" element={<UploadPage />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
